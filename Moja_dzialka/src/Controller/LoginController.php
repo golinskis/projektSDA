2b7b688619e048f5b156601b4158b166a0f3da25
Alexander Linke <alexanderlinke1995@gmail.com>
2018-05-12 10:37:04 +0200

    uzupełnienie controllera o render twiga

--- Moja_dzialka/src/Controller/LoginController.php
+++ Moja_dzialka/src/Controller/LoginController.php
@@ -6,5 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LoginController extends Controller
 {
-    
+    public function login()
+    {
+        return $this->render('login/login.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
 }
