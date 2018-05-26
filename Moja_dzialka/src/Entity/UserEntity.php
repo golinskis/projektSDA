cdf00f0656ccbf033ed7fe68cd3561c2247feb62
Alexander Linke <alexanderlinke1995@gmail.com>
2018-05-26 13:19:38 +0200

    zakomentowanie starego entity i podmiana na FOS

--- Moja_dzialka/src/Entity/UserEntity.php
+++ Moja_dzialka/src/Entity/UserEntity.php
@@ -2,120 +2,142 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+// use Doctrine\Common\Collections\ArrayCollection;
+// use Doctrine\Common\Collections\Collection;
+// use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
  */
-class UserEntity
+class User extends BaseUser
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @ORM\Id
      * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $password;
-
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\CommentEntity", mappedBy="relation")
-     */
-    private $comment;
+    protected $id;
 
     public function __construct()
     {
-        $this->comment = new ArrayCollection();
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|CommentEntity[]
-     */
-    public function getComment(): Collection
-    {
-        return $this->comment;
-    }
-
-    public function addComment(CommentEntity $comment): self
-    {
-        if (!$this->comment->contains($comment)) {
-            $this->comment[] = $comment;
-            $comment->setRelation($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(CommentEntity $comment): self
-    {
-        if ($this->comment->contains($comment)) {
-            $this->comment->removeElement($comment);
-            // set the owning side to null (unless already changed)
-            if ($comment->getRelation() === $this) {
-                $comment->setRelation(null);
-            }
-        }
-
-        return $this;
-    }
-    public function __toString()
-    {
-        return $this->getName();
+        parent::__construct();
+        // your own logic
     }
 }
+
+// /**
+//  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+//  */
+// class UserEntity
+// {
+//     /**
+//      * @ORM\Id()
+//      * @ORM\GeneratedValue()
+//      * @ORM\Column(type="integer")
+//      */
+//     private $id;
+
+//     /**
+//      * @ORM\Column(type="string", length=255)
+//      */
+//     private $name;
+
+//     /**
+//      * @ORM\Column(type="string", length=255)
+//      */
+//     private $email;
+
+//     /**
+//      * @ORM\Column(type="string", length=255)
+//      */
+//     private $password;
+
+//     /**
+//      * @ORM\OneToMany(targetEntity="App\Entity\CommentEntity", mappedBy="relation")
+//      */
+//     private $comment;
+
+//     public function __construct()
+//     {
+//         $this->comment = new ArrayCollection();
+//     }
+
+//     public function getId()
+//     {
+//         return $this->id;
+//     }
+
+//     public function getName(): ?string
+//     {
+//         return $this->name;
+//     }
+
+//     public function setName(string $name): self
+//     {
+//         $this->name = $name;
+
+//         return $this;
+//     }
+
+//     public function getEmail(): ?string
+//     {
+//         return $this->email;
+//     }
+
+//     public function setEmail(string $email): self
+//     {
+//         $this->email = $email;
+
+//         return $this;
+//     }
+
+//     public function getPassword(): ?string
+//     {
+//         return $this->password;
+//     }
+
+//     public function setPassword(string $password): self
+//     {
+//         $this->password = $password;
+
+//         return $this;
+//     }
+
+//     /**
+//      * @return Collection|CommentEntity[]
+//      */
+//     public function getComment(): Collection
+//     {
+//         return $this->comment;
+//     }
+
+//     public function addComment(CommentEntity $comment): self
+//     {
+//         if (!$this->comment->contains($comment)) {
+//             $this->comment[] = $comment;
+//             $comment->setRelation($this);
+//         }
+
+//         return $this;
+//     }
+
+//     public function removeComment(CommentEntity $comment): self
+//     {
+//         if ($this->comment->contains($comment)) {
+//             $this->comment->removeElement($comment);
+//             // set the owning side to null (unless already changed)
+//             if ($comment->getRelation() === $this) {
+//                 $comment->setRelation(null);
+//             }
+//         }
+
+//         return $this;
+//     }
+//     public function __toString()
+//     {
+//         return $this->getName();
+//     }
+// }
